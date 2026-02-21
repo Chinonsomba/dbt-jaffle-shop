102bc3d588a8aefa23182507ba70157c7b19bdbc
Chinonso Mba <chvnonso@gmail.com>
2026-02-21 03:20:19 +0000

    Organized a finance mart structure, built the fact table to house the metrics, and added Stripe staging to feed clean payment data into the pipeline.

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -1,46 +1,35 @@
+{{ config(materialized='table') }}
 
 with customers as (
 
-select * from {{ ref('stg_jaffle_shop__customers') }}
+    select *
+    from {{ ref('stg_jaffle_shop__customers') }}
 
 ),
 
 orders as (
 
-    select * from {{ ref('stg_jaffle_shop__orders') }}
+    select *
+    from {{ ref('fct_orders') }}
 
 ),
 
-customer_orders as (
+customer_metrics as (
 
     select
-        customer_id,
+        c.customer_id,
+        c.first_name,
+        c.last_name,
 
-        min(order_date) as first_order_date,
-        max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
+        count(o.order_id) as number_of_orders,
+        coalesce(sum(o.amount), 0) as lifetime_value
 
-    from orders
+    from customers c
+    left join orders o
+        on c.customer_id = o.customer_id
 
-    group by 1
-
-),
-
-
-final as (
-
-    select
-        customers.customer_id,
-        customers.first_name,
-        customers.last_name,
-        customer_orders.first_order_date,
-        customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
-
-    from customers
-
-    left join customer_orders using (customer_id)
-
+    group by 1,2,3
 )
 
-select * from final
+select *
+from customer_metrics
---
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -0,0 +1,11 @@
+{{ config(materialized='view') }}
+
+select
+    id as payment_id,
+    orderid as order_id,
+    paymentmethod as payment_method,
+    amount / 100.0 as amount,
+    created as created_at
+
+from {{ source('stripe', 'payment') }}
+where status = 'success'
